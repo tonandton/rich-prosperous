8cb13176bd6aad239abe465a3a337b6fc4770978
tonandton <www.sorasakok@gmail.com>
2024-10-08 19:41:11 +0700

    update text service

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -8,36 +8,40 @@ const Service = () => {
       <div className="pt-28 px-4 container mx-auto">
         <div className="text-center space-y-5">
           <h2 className="text-4xl font-bold font-secondary text-black">
-            What Can We Do Together
+            รับสร้างโกดังอสังหาริมทรัพย์
           </h2>
           <p className="md:w-1/2 mx-auto">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quod
-            sint repudiandae. Corporis unde dicta tempore numquam nihil
-            doloribus sequi, aut corrupti iste, ullam ab alias aspernatur
-            nesciunt eum quisquam!
+            ม้านั่งแตงกวา โฮสเตสเมจิคดั๊มพ์ ลอร์ดคอรัปชันเปียโน เฟิร์มรีสอร์ต
+            ไอเดียเบบี้นินจา ดาวน์ป๊อกโปรเจกต์คอร์ส อึมครึมโซนโหลนจูเนียร์ไชน่า
+            เดบิตหลินจือโทรอุปสงค์ ซินโดรมตัวตนสะเด่าแลนด์งั้น โหงวโนติสวิปรุสโซ
+            หล่อฮังก้วยเอ็นจีโอแพ็ค แฟรีเซ็นเตอร์ปิยมิตรควิกเอสเพรสโซ
+            เมจิคเซฟตี้ติวเตอร์เรตติ้งไฮบริด ผิดพลาดเสือโคร่งนาฏยศาลาโอ้ย
+            อินเตอร์ คาสิโนคอร์ปอเรชั่น
           </p>
         </div>
         {/* service category */}
         <div className="py-12 md:w-4/5 mx-auto">
           <Tabs>
             <TabList className="flex flex-wrap justify-between items-center md:gap-8 gap-4">
-              <Tab>Couple Counseling</Tab>
-              <Tab>Parenting Skills</Tab>
-              <Tab>Feeling Stuck</Tab>
-              <Tab>Self-Confidence</Tab>
+              <Tab>หัวข้อที่ 1</Tab>
+              <Tab>หัวข้อที่ 2</Tab>
+              <Tab>หัวข้อที่ 3</Tab>
+              <Tab>หัวข้อที่ 4</Tab>
             </TabList>
 
             <TabPanel>
               <div className="flex flex-col md:flex-row gap-8 mt-8">
                 <div className="md:w-1/2 bg-white rounded-lg p-12 font-secondary">
                   <h3 className="text-3xl font-semibold text-blue-700 mb-4">
-                    Couple Counseling
+                    หัวข้อที่ 1
                   </h3>
                   <p className="mb-8">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Consectetur incidunt eligendi eum libero sequi repellendus,
-                    culpa quia ullam aliquam quas atque ducimus nostrum commodi
-                    qui adipisci! Quae officia similique voluptatem!
+                    มวลชนซามูไร ออร์เดอร์สัมนาคอมเมนท์พอเพียงคาวบอย ทัวร์เอาต์
+                    ฮ็อตซินโดรมฮ็อตแฟนตาซี นายแบบล้มเหลวซัมเมอร์
+                    แดนเซอร์สจ๊วตเคส คาร์โก้วิดีโอสังโฆม้านั่ง เซ็นเซอร์
+                    นิรันดร์รวมมิตรแมคเคอเรลยะเยือกบลูเบอร์รี วอร์รูม
+                    อพาร์ตเมนต์สเตย์ป๊อปบึ้มมายองเนส สหัสวรรษออกแบบเบนโลพล็อต
+                    เหมย บร็อคโคลีว้อยโคโยตี โกะบู๊ซีนเซาท์ จ๊าบแมนชั่น
                   </p>
                   <h4 className="text-xl font-medium text-black md-4">
                     Benefits
@@ -62,13 +66,16 @@ const Service = () => {
               <div className="flex flex-col md:flex-row gap-8 mt-8">
                 <div className="md:w-1/2 bg-white rounded-lg p-12 font-secondary">
                   <h3 className="text-3xl font-semibold text-blue-700 mb-4">
-                    Parenting Skills
+                    หัวข้อที่ 2
                   </h3>
                   <p className="mb-8">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Consectetur incidunt eligendi eum libero sequi repellendus,
-                    culpa quia ullam aliquam quas atque ducimus nostrum commodi
-                    qui adipisci! Quae officia similique voluptatem!
+                    แอลมอนด์ โง่เขลารากหญ้า เกย์โบรชัวร์เจไดวินเซ็กซ์
+                    ติวซิตี้สเตเดียม งี้แคนูติ่มซำมิลค์เลคเชอร์
+                    บอกซ์ภควัมบดีคอร์รัปชันเอ็นจีโอ
+                    กระดี๊กระด๊าแหววรายชื่อแรลลีลาติน เบอร์รีคอร์รัปชัน
+                    ซิตี้กุนซือ มิลค์ ทัวริสต์นายพรานคอนแท็ค
+                    พงษ์ลิมูซีนไนท์มาร์ชโปรเจ็ค ฮาร์ดไฮเวย์ ซูโม่ สติกเกอร์โบว์
+                    อัลตราพะเรอ
                   </p>
                   <h4 className="text-xl font-medium text-black md-4">
                     Benefits
@@ -92,14 +99,17 @@ const Service = () => {
             <TabPanel>
               <div className="flex flex-col md:flex-row gap-8 mt-8">
                 <div className="md:w-1/2 bg-white rounded-lg p-12 font-secondary">
-                  <h3 className="text-3xl font-semibold text-blue-700 mb-4">
-                    Feeling Stuck
-                  </h3>
+                  <h3 className="text-3xl font-semibold text-blue-700 mb-4"></h3>
                   <p className="mb-8">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Consectetur incidunt eligendi eum libero sequi repellendus,
-                    culpa quia ullam aliquam quas atque ducimus nostrum commodi
-                    qui adipisci! Quae officia similique voluptatem!
+                    ซูโม่เพรียวบางสมิติเวชหมั่นโถว พุทธภูมิ ราเมน
+                    อีสต์เอ็นทรานซ์ซานตาคลอสสเตริโอธุรกรรม
+                    พอเพียงสแล็กโซลาร์ฮิช็อป ไลน์ไคลแม็กซ์คอร์รัปชันกุนซือ
+                    โอยัวะโปรเจคท์สปอต เย้วแดนเซอร์แจ๊กเก็ตยังไง
+                    แฟ็กซ์บรรพชนทอร์นาโดทรูหล่อฮังก้วย
+                    ปัจฉิมนิเทศสไตล์สเตเดียมเอาท์ดอร์บูม
+                    เมจิคเทวาธิราชปิยมิตรสปาย อาข่าฉลุยตัวเองติ๋ม
+                    จิ๊กโก๋เซอร์อิ่มแปร้ เห็นด้วยฮาลาลเหี่ยวย่นน็อกคอนโดมิเนียม
+                    อุเทนแอโรบิคฟอร์มสะบึมเอสเปรสโซ ซีรีส์คันธาระ
                   </p>
                   <h4 className="text-xl font-medium text-black md-4">
                     Benefits
@@ -124,13 +134,15 @@ const Service = () => {
               <div className="flex flex-col md:flex-row gap-8 mt-8">
                 <div className="md:w-1/2 bg-white rounded-lg p-12 font-secondary">
                   <h3 className="text-3xl font-semibold text-blue-700 mb-4">
-                    Self-Confidence
+                    หัวที่ 4
                   </h3>
                   <p className="mb-8">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Consectetur incidunt eligendi eum libero sequi repellendus,
-                    culpa quia ullam aliquam quas atque ducimus nostrum commodi
-                    qui adipisci! Quae officia similique voluptatem!
+                    ซีดาน เวสต์ เทคโนแอ็กชั่นตังค์ สโลว์เทรนด์วานิลลา
+                    เจ๊าะแจ๊ะเซอร์วิสฟลุค เธคสแตนดาร์ดเสือโคร่งไทม์
+                    สามช่าเกมส์ไบเบิล คลิปโอเวอร์ช็อปเปอร์แลนด์
+                    บู๊สุริยยาตรแฟร์แอสเตอร์ทอร์นาโด รีโมตซาดิสต์ซูฮก โนติส
+                    ไฟลท์ไตรมาส แฟรี่แล็บ เอาท์ดอร์ฮิปโปบาร์บี้เยอร์บีราจิตเภท
+                    เซ็กซ์แบตไมค์ง่าว มหภาคโปรเจ็กต์
                   </p>
                   <h4 className="text-xl font-medium text-black md-4">
                     Benefits
